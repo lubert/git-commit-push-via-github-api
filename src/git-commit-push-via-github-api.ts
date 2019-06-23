fb5baaaff46646a73856e918472172fc89ef8ae6
Lu Zhu <misterzhu@gmail.com>
2019-06-23 09:51:57 -0700

    Update octokit usage

--- src/git-commit-push-via-github-api.ts
+++ src/git-commit-push-via-github-api.ts
@@ -3,128 +3,112 @@ import * as GitHubApi from "@octokit/rest";
 const debug = require("debug")("git-commit-push-via-github-api");
 const GITHUB_API_TOKEN = process.env.GITHUB_API_TOKEN;
 const getReferenceCommit = function(github: GitHubApi, options: GitCommitPushOptions) {
-    return new Promise((resolve, reject) => {
-        github.git.getRef(
-            {
-                owner: options.owner,
-                repo: options.repo,
-                ref: options.fullyQualifiedRef
-            },
-            (err, res) => {
-                if (err) {
-                    debug("getReferenceCommit Error", JSON.stringify(err, null, "  "));
-                    return reject(err);
-                }
-                debug("getReferenceCommit Response: %O", res);
-                return resolve({ referenceCommitSha: res.data.object.sha });
-            }
-        );
-    });
+    return github.git
+        .getRef({
+            owner: options.owner,
+            repo: options.repo,
+            ref: options.fullyQualifiedRef
+        })
+        .then(res => {
+            debug("getReferenceCommit Response: %O", res);
+            return { referenceCommitSha: res.data.object.sha };
+        })
+        .catch(err => {
+            debug("getReferenceCommit", JSON.stringify(err, null, "  "));
+            throw err;
+        });
 };
 
 const createTree = function(github: GitHubApi, options: GitCommitPushOptions, data: any) {
-    return new Promise((resolve, reject) => {
-        const promises = options.files.map(file => {
-            if (typeof file.path === "string" && typeof file.content === "string") {
-                return github.git
-                    .createBlob({
-                        owner: options.owner,
-                        repo: options.repo,
-                        content: file.content,
-                        encoding: "utf-8"
-                    })
-                    .then((blob: any) => {
-                        return {
-                            sha: blob.data.sha,
-                            path: file.path,
-                            mode: "100644",
-                            type: "blob"
-                        };
-                    });
-            } else if (typeof file.path === "string" && Buffer.isBuffer(file.content)) {
-                return github.git
-                    .createBlob({
-                        owner: options.owner,
-                        repo: options.repo,
-                        content: file.content.toString("base64"),
-                        encoding: "base64"
-                    })
-                    .then((blob: any) => {
-                        return {
-                            sha: blob.data.sha,
-                            path: file.path,
-                            mode: "100644",
-                            type: "blob"
-                        };
-                    });
-            }
-            throw new Error(`This file can not handled: ${file}`);
-        });
-        return Promise.all(promises).then(files => {
-            debug("files: %O", files);
-            // TODO: d.ts bug?
-            github.git.createTree(
-                {
+    const promises = options.files.map(file => {
+        if (typeof file.path === "string" && typeof file.content === "string") {
+            return github.git
+                .createBlob({
                     owner: options.owner,
                     repo: options.repo,
-                    tree: files,
-                    base_tree: data.referenceCommitSha
-                } as any,
-                (err: any, res: any) => {
-                    if (err) {
-                        debug("createTree", JSON.stringify(err, null, "  "));
-                        return reject(err);
-                    }
-                    debug("createTree Response: %O", res);
-                    return resolve(Object.assign(data, { newTreeSha: res.data.sha }));
-                }
-            );
-        });
+                    content: file.content,
+                    encoding: "utf-8"
+                })
+                .then((blob: any) => {
+                    return {
+                        sha: blob.data.sha,
+                        path: file.path,
+                        mode: "100644",
+                        type: "blob"
+                    };
+                });
+        } else if (typeof file.path === "string" && Buffer.isBuffer(file.content)) {
+            return github.git
+                .createBlob({
+                    owner: options.owner,
+                    repo: options.repo,
+                    content: file.content.toString("base64"),
+                    encoding: "base64"
+                })
+                .then((blob: any) => {
+                    return {
+                        sha: blob.data.sha,
+                        path: file.path,
+                        mode: "100644",
+                        type: "blob"
+                    };
+                });
+        }
+        throw new Error(`This file can not handled: ${file}`);
+    });
+    return Promise.all(promises).then(files => {
+        debug("files: %O", files);
+        // TODO: d.ts bug?
+        return github.git
+            .createTree({
+                owner: options.owner,
+                repo: options.repo,
+                tree: files,
+                base_tree: data.referenceCommitSha
+            } as any)
+            .then(res => {
+                debug("createTree Response: %O", res);
+                return Object.assign(data, { newTreeSha: res.data.sha });
+            })
+            .catch(err => {
+                debug("createTree", JSON.stringify(err, null, "  "));
+                throw err;
+            });
     });
 };
 
 const createCommit = function(github: GitHubApi, options: GitCommitPushOptions, data: any) {
-    return new Promise((resolve, reject) => {
-        github.git.createCommit(
-            {
-                owner: options.owner,
-                repo: options.repo,
-                message: options.commitMessage || "commit",
-                tree: data.newTreeSha,
-                parents: [data.referenceCommitSha]
-            },
-            (err, res) => {
-                if (err) {
-                    debug("createCommit", JSON.stringify(err, null, "  "));
-                    return reject(err);
-                }
-                debug("createCommit Response: %O", res);
-                return resolve(Object.assign(data, { newCommitSha: res.data.sha }));
-            }
-        );
-    });
+    return github.git
+        .createCommit({
+            owner: options.owner,
+            repo: options.repo,
+            message: options.commitMessage || "commit",
+            tree: data.newTreeSha,
+            parents: [data.referenceCommitSha]
+        })
+        .then(res => {
+            debug("createCommit Response: %O", res);
+            return Object.assign(data, { newCommitSha: res.data.sha });
+        })
+        .catch(err => {
+            debug("createCommit", JSON.stringify(err, null, "  "));
+            throw err;
+        });
 };
 
 const updateReference = function(github: GitHubApi, options: GitCommitPushOptions, data: any) {
-    return new Promise((resolve, reject) => {
-        github.git.updateRef(
-            {
-                owner: options.owner,
-                repo: options.repo,
-                ref: options.fullyQualifiedRef,
-                sha: data.newCommitSha,
-                force: options.forceUpdate
-            },
-            (err, data) => {
-                if (err) {
-                    debug("updateReference", JSON.stringify(err, null, "  "));
-                    return reject(err);
-                }
-                debug("updateReference Response: %O", data);
-                return resolve(data);
-            }
-        );
-    });
+    return github.git
+        .updateRef({
+            owner: options.owner,
+            repo: options.repo,
+            ref: options.fullyQualifiedRef,
+            sha: data.newCommitSha,
+            force: options.forceUpdate
+        })
+        .catch(err => {
+            debug("updateReference", JSON.stringify(err, null, "  "));
+            throw err;
+        });
 };
 
 export interface GitCommitPushOptions {
@@ -148,7 +132,7 @@ export const gitCommitPush = (options: GitCommitPushOptions) => {
     if (!token) {
         throw new Error(`token is not defined`);
     }
-    const gitHub = new GitHubApi({auth: token});
+    const gitHub = new GitHubApi({ auth: token });
     const filledOptions = {
         owner: options.owner,
         repo: options.repo,
